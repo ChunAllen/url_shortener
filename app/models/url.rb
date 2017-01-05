56a101c8ca0ff502ec6f232a07ae57049f37a42f
Allen Chun <allen.chun@hotmail.com>
2017-01-05 08:09:16 +0800

    Adds url model

---
+++ app/models/url.rb
@@ -0,0 +1,13 @@
+class Url < ApplicationRecord
+
+  validates :given_url, presence: true
+
+  after_create :generate_slug
+
+  private
+
+  def generate_slug
+    self.slug = Digest::MD5.hexdigest("#{given_url}/#{id}").slice(0..6)
+  end
+
+end
