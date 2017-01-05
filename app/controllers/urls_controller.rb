3fe4bc9a973c5ff5c703486fd77f28ba4a3f7b1e
Allen Chun <allen.chun@hotmail.com>
2017-01-05 11:00:42 +0800

    Adds creation of url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,7 +1,27 @@
 class UrlsController < ApplicationController
 
   def index
-    @urls = Url.all
+    @urls = UrlDecorator.wrap_all(Url.all)
   end
+
+	def new
+		@url = Url.new
+	end
+
+	def create
+		binding.pry
+		@url = Url.new(url_params)
+		if @url.save
+			flash[:notice] = 'New URL has been shortened'
+		else
+			flash[:error] = @url.errors.full_messages.to_sentence
+		end
+	end
+
+	private
+
+	def url_params
+		params.require(:url).permit(:given_url)
+	end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def flash_class(type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }[type.to_sym] || type.to_s
+  end
+
+  def css_class_for_controller_action(controller)
+    "#{controller.controller_path.parameterize.tr('-', '_')} #{controller.action_name}"
+  end
+
 end
